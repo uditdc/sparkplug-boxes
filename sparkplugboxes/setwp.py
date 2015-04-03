d0d24a814ab47c88a3fa58169e2dfff360d48281
Udit <coder364@gmail.com>
2015-04-03 12:08:10 +0800

    Setwp file formatting

--- sparkplugboxes/setwp.py
+++ sparkplugboxes/setwp.py
@@ -9,85 +9,87 @@ from socketio.namespace import BaseNamespace
 
 
 class SetwpNamespace(BaseNamespace):
-    """
-    Class to setup a WordPress Installation
-    This class is served using zerorpc
-    """
+	"""
+	Class to setup a WordPress Installation
+	This class is served using zerorpc
+	"""
 
-    def initialize(self):
-        """
-        Initialization function for SetupWP
-        This function saves the
-        """
+	def initialize(self):
+		"""
+		Initialization function for SetupWP
+		This function saves the
+		"""
 
-        self.FOLDER = os.path.abspath('/var/www/boxes')
+		self.FOLDER = os.path.abspath('/var/www/boxes')
 
-        self.MYSQL_PATH = '/usr/bin/mysql'
-        self.WP = {
-            'URL': 'http://wordpress.org/latest.zip',
-            'SALT': 'https://api.wordpress.org/secret-key/1.1/salt/'
-        }
+		self.MYSQL_PATH = '/usr/bin/mysql'
+		self.WP = {
+		'URL': 'http://wordpress.org/latest.zip',
+		'SALT': 'https://api.wordpress.org/secret-key/1.1/salt/'
+		}
 
-        self.emit('ready', {'message': 'Setwp Server is up'})
+		self.emit('ready', {'message': 'Setwp Server is up'})
 
-    def on_files_setup(self, box_id):
-        """
-        This function will download the wordpress
-        zip file and call the unzip function if
-        the download was successful.
+	def on_files_setup(self, box_id):
+		"""
+		This function will download the wordpress
+		zip file and call the unzip function if
+		the download was successful.
 
-        Emit Order
+		Emit Order
 
-        1. Download Started ...
-        2. Download Complete ...
+		1. Download Started ...
+		2. Download Complete ...
 
-        :param box_id: The box id is used to name the directory
-        :return: Success or Failure flag
-        """
-	print "Function Called"
-        # Create a directory for this installation
+		:param box_id: The box id is used to name the directory
+		:return: Success or Failure flag
+		"""
+		print "Function Called"
 
-        dest_path = self.FOLDER + '/' + box_id
+		# Create a directory for this installation
 
-        if not os.path.exists(dest_path):
-            os.makedirs(dest_path)
-	print "destination path :" + dest_path
-        self.emit('status', {
-            'icon': 'cloud-download',
-            'message': 'Download Started'
-        })
+		dest_path = self.FOLDER + '/' + box_id
 
-        # Download the zip archive from the repo
-        archive = urllib.urlretrieve(
-            self.WP['URL'],
-            '%s/latest.zip' % dest_path
-        )
+		if not os.path.exists(dest_path):
+			os.makedirs(dest_path)
+			print "destination path :" + dest_path
 
-        if archive:
-            # The download was successful
-            # Call the unzip function
-            self.unzip(dest_path)
-        else:
-            # The Download was unsuccessful
-            # Return a failure flag
-            print 'Download Failed'
-            self.emit('status', {'message': ('failed on %s' % dest_path)})
+			self.emit('status', {
+				'icon': 'cloud-download',
+				'message': 'Download Started'
+			})
 
-    def unzip(self, dest_path):
-        """
-        This function will unzip the previously downloaded
-        zip file.
+		# Download the zip archive from the repo
+		archive = urllib.urlretrieve(
+			self.WP['URL'],
+			'%s/latest.zip' % dest_path
+		)
 
-        :param dest_path: The dest_path passes the zip file location
-        :return: Success or Failure flag
-        """
+		if archive:
+			# The download was successful
+			# Call the unzip function
+			self.unzip(dest_path)
+		else:
+			# The Download was unsuccessful
+			# Return a failure flag
+			print 'Download Failed'
+			self.emit('status', {'message': ('failed on %s' % dest_path)})
 
-        archive = zipfile.ZipFile('%s/latest.zip' % dest_path, 'r')
-        archive.extractall(dest_path)
+	def unzip(self, dest_path):
+		"""
+		This function will unzip the previously downloaded
+		zip file.
 
-        self.emit('status', {
-            'icon': 'cloud-download',
-            'message': 'Download & Unzip complete '
-        })
+		:param dest_path: The dest_path passes the zip file location
+		:return: Success or Failure flag
+		"""
 
-        print 'Unzip Completed'
+		archive = zipfile.ZipFile('%s/latest.zip' % dest_path, 'r')
+		archive.extractall(dest_path)
+
+		self.emit('status', {
+			'icon': 'cloud-download',
+			'message': 'Download & Unzip complete '
+			})
+
+		print 'Unzip Completed'
